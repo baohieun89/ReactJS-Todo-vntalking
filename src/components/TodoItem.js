170a50ab3b355540f025ff9ae36a04ea8aca8d0c
Santjago <baohieu.n89@gmail.com>
2020-08-18 13:56:07 +0700

    day2 add checkbox

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,10 +1,12 @@
 import React from "react";
 class TodoItem extends React.Component {
-    render() {
-        return (
-            <li className="todo-item">{this.props.todo.title}</li>
-        );
-    }
+	render() {
+		return (
+			<li className="todo-item">
+			<input type="checkbox" checked={this.props.todo.complete}/>	{this.props.todo.title}
+			</li>
+		);
+	}
 }
 
 export default TodoItem;
--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import "./style/App.css"
 
 import TodoApp from "./components/TodoApp"
-const element = <h1>Hello from VNTALKING 1</h1>;
+//const element = <h1>Hello from VNTALKING 1</h1>;
 const root = document.getElementById("root");
 const todoApp = <TodoApp />;
 ReactDOM.render(todoApp, root);
